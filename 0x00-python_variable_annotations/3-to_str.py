707f07f89e08d486cd9ff3f26fd0cbe7fb914655
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-07 00:16:14 +0100

    implemented float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that takes a float and returns the string representation of it.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that takes a float and returns the string representation of it.
+    """
+    return str(n)
