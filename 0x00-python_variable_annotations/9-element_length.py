3f13a06c9f719ec541c3ea084248a165f6ff5166
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-07 01:17:56 +0100

    added docstrings

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,8 @@ from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Method that takes a list of strings and returns a list of tuples
+    containing the string and its length.
+    """
     return [(i, len(i)) for i in lst]
