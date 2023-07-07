5a6a936a156907a702c8502e46b195b2e17ffbb1
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-07 01:41:17 +0100

    implemented mapping annotation

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that takes a dict and a key and returns the value, None if missing.
+"""
+from typing import Any, Mapping, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                        default: Union[T, None] = None) -> Union[Any, T]:
+        """
+        Method that takes a dict and a key and returns the value, None if missing.
+        """
+        if key in dct:
+            return dct[key]
+        else:
+            return default
