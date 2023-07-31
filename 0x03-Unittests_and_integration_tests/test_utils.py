47cffba9512c46e7d5892f50ab81c69bcffbb6ed
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-01 00:18:03 +0100

    handled patch with parameterized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Test utils module. """
 import unittest
+from unittest.mock import patch
 from utils import access_nested_map
+from utils import get_json
 from parameterized import parameterized
 
 
@@ -25,6 +27,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Tests the Access Nested Map Exception method. """
         with self.assertRaises(expected_result):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Get Json Test Class. """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ Test the Get Json method. """
+        mock_response = mock_get.return_value
+        mock_response.json.return_value = test_payload
+
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
